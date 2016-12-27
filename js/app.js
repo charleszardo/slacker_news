736c923f1724dab6fd77767812aec8ecc0284069
Dave Charnuska <davecharnuska@gmail.com>
2016-12-27 12:24:40 -0500

    implement voting

--- js/app.js
+++ js/app.js
@@ -2,10 +2,20 @@ angular.module('slackerNews', [])
   .controller('MainCtrl', [
     '$scope', function($scope) {
       $scope.posts = []
+
       $scope.addPost = function () {
         if (!$scope.title || $scope.title === '') { return; }
         $scope.posts.push({ title: $scope.title, upvotes: 0 });
         $scope.title = '';
+      }
+
+      $scope.vote = function(post, val) {
+        console.log(val)
+        if (val === 1) {
+          post.upvotes++
+        } else {
+          post.upvotes -= 1
+        }
       }
     }
   ])
