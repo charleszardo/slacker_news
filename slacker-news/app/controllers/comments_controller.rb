f76eb396ce403fd651245c85e52871ddb76fa29d
Dave Charnuska <davecharnuska@gmail.com>
2016-12-28 15:25:31 -0500

    add downvotes action, fix server side upvote update logic

--- slacker-news/app/controllers/comments_controller.rb
+++ slacker-news/app/controllers/comments_controller.rb
@@ -4,13 +4,22 @@ class CommentsController < ApplicationController
     comment = post.comments.new(comment_params)
     comment.upvotes = 0
     comment.save
-    
+
     respond_with post, comment
   end
 
   def upvote
     post = Post.find(params[:post_id])
     comment = post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+
+    respond_with post, comment
+  end
+
+  def downvote
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.decrement!(:upvotes)
 
     respond_with post, comment
   end
